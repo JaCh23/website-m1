b58fd3cd0e3b8e057270168ecc9a753b15a683cf
JaCh23 <jach2355@gmail.com>
2021-05-31 02:05:50 +0800

    Change wrt 300521

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBLRMMmtE0qEtt6B5bev6JjkJ5cYDS_aR8",
+  authDomain: "orbital-m1.firebaseapp.com",
+  projectId: "orbital-m1",
+  storageBucket: "orbital-m1.appspot.com",
+  messagingSenderId: "456313656003",
+  appId: "1:456313656003:web:87b50cd0f2bc77765dc876",
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
+import { FirebaseAuthProvider } from "@react-firebase/auth";
+import { firebase } from "@firebase/app";
+import "@firebase/auth";
+import { config } from "./config/firebase";
+
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <FirebaseAuthProvider {...config} firebase={firebase}>
+      <App />
+    </FirebaseAuthProvider>
   </StrictMode>,
   rootElement
 );
